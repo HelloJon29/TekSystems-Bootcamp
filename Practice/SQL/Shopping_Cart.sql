052ddf3f8748b7b9fa378c615ba691d94d8bc8b3
HelloJon29 <jonkim1996@gmail.com>
2022-02-15 18:12:18 -0600

    Updated shopping cart table

--- Practice/SQL/Shopping_Cart.sql
+++ Practice/SQL/Shopping_Cart.sql
@@ -1,15 +1,16 @@
-/*Here FOR testing reasons*/
-DROP TABLE IF EXISTS `shopping cart`;
+/*Here for testing reasons*/
+DROP TABLE IF EXISTS `shoppingcart`;
 /* Create the table*/
-CREATE TABLE `shopping cart` (
+CREATE TABLE `shoppingcart` (
 	`cart_id` INT(11) NOT NULL AUTO_INCREMENT,
-	`product_name` VARCHAR(255) NOT NULL,
+	`product` VARCHAR(255) NOT NULL,
 	`qty` INT(11) NOT NULL,
 	`customer_id` INT(11) NOT NULL,
 	PRIMARY KEY (`cart_id`),
 	CONSTRAINT `fk_customer_id`
 		FOREIGN KEY (`customer_id`)
 		REFERENCES `customers` (`customerNumber`),
-	CONSTRAINT `fk_product_name`
-		FOREIGN KEY (`product_name`) REFERENCES `products` (`productName`)
+	CONSTRAINT `fk_product`
+		FOREIGN KEY (`product`) 
+		REFERENCES `products` (`productCODE`)
 );
