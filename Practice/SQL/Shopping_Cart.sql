e0e38d4e40096c171aea0717087965ade5d320a9
HelloJon29 <jonkim1996@gmail.com>
2022-02-16 20:32:55 -0600

    New exercise and updated shopping cart table

--- Practice/SQL/Shopping_Cart.sql
+++ Practice/SQL/Shopping_Cart.sql
@@ -2,15 +2,16 @@
 DROP TABLE IF EXISTS `shoppingcart`;
 /* Create the table*/
 CREATE TABLE `shoppingcart` (
-	`cart_id` INT(11) NOT NULL AUTO_INCREMENT,
-	`product` VARCHAR(255) NOT NULL,
+	`cartNumber` INT(11) NOT NULL AUTO_INCREMENT,
+	`customerNumber` INT(11) NOT NULL,
+	`productCodE` VARCHAR(50) NOT NULL,
 	`qty` INT(11) NOT NULL,
-	`customer_id` INT(11) NOT NULL,
-	PRIMARY KEY (`cart_id`),
-	CONSTRAINT `fk_customer_id`
-		FOREIGN KEY (`customer_id`)
-		REFERENCES `customers` (`customerNumber`),
-	CONSTRAINT `fk_product`
-		FOREIGN KEY (`product`) 
-		REFERENCES `products` (`productCODE`)
-);
+	PRIMARY KEY (`cartNumber`),
+	KEY `INDEX` (`customerNumber`,`productCode`),
+	CONSTRAINT `fk_customerNumber` FOREIGN KEY (`customerNumber`) REFERENCES `customers`(`customerNumber`),
+	CONSTRAINT `fk_productCode` FOREIGN KEY (`productCode`) REFERENCES `products` (`productCode`)
+) ENGINE = INNODB;
+
+/*Insert test data into table, the cartNumber will automatically populate*/
+INSERT INTO `shoppingcart`(`customerNumber`,`productCode`,`qty`) VALUES
+	(103,'S10_1949',22);
