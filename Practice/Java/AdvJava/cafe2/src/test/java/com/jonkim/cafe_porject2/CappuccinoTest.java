cf0f4d76b8794c81fb0a109a328ce91cc8b21125
HelloJon29 <jonkim1996@gmail.com>
2022-03-07 08:45:44 -0600

    Testing made for cafe 2

--- Practice/Java/AdvJava/cafe2/src/test/java/com/jonkim/cafe_porject2/CappuccinoTest.java
+++ Practice/Java/AdvJava/cafe2/src/test/java/com/jonkim/cafe_porject2/CappuccinoTest.java
@@ -3,19 +3,23 @@ package com.jonkim.cafe_porject2;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class CappuccinoTest {
 
     @Test
     void calculateSubTotal() {
         Cappuccino jonsOrder = new Cappuccino("testCap", 2.50, "Sweet",true,true);
-        Cappuccino mosOrder = new Cappuccino("Mos", 3.50, "Sweet",false,true);
+        Cappuccino jonsOrder2 = new Cappuccino("testCap", 2.50, "Sweet",true,true);
+        Cappuccino mosOrder = new Cappuccino("Mos", 3.50, "Sweet",true,true);
 
         mosOrder.setPrice(2.50);
         mosOrder.setQuantity(1);
         jonsOrder.setQuantity(1);
+        jonsOrder2.setQuantity(1);
+        System.out.println(jonsOrder2.toString());
+        System.out.println(jonsOrder.toString());
 
+        assertThat(jonsOrder).isEqualTo(jonsOrder2);
         assertThat(jonsOrder.calculateSubTotal()).isEqualTo(mosOrder.calculateSubTotal());
         assertThat(jonsOrder.isPeppermint()).isEqualTo(mosOrder.isPeppermint());
     }
