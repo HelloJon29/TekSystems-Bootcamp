103df8130435d08fd422cc269dbe9c0f47ad7c7e
HelloJon29 <jonkim1996@gmail.com>
2022-03-23 19:04:31 -0500

    finished sms sba, main runner completed

--- Practice/Java/adv_java_sba/SMS/src/main/java/jpa/mainrunner/SMSRunner.java
+++ Practice/Java/adv_java_sba/SMS/src/main/java/jpa/mainrunner/SMSRunner.java
@@ -1,16 +1,17 @@
 package jpa.mainrunner;
 
+import jpa.dao.CourseDAO;
+import jpa.dao.StudentDAO;
 import jpa.service.CourseService;
 import jpa.service.StudentService;
 
 import java.util.InputMismatchException;
-import java.util.Locale;
 import java.util.Scanner;
 
 public class SMSRunner {
     // instantiate service classes and a Scanner for user input
-    private static StudentService studentService = new StudentService();
-    private static CourseService courseService = new CourseService();
+    private static StudentDAO studentService = new StudentService();
+    private static CourseDAO courseService = new CourseService();
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
@@ -33,7 +34,7 @@ public class SMSRunner {
             input.nextLine();
             switch(choice) {
                 case 1:
-                    //
+                    fetchStudent();
                     break;
                 case 2:
                     // close scanner here to prevent memory leak, this is the end of the app
@@ -59,7 +60,7 @@ public class SMSRunner {
 
     // static method to print register course menu to console
     public static void registerCourseMenu(){
-        System.out.println("1. Register to Class\n" +
+        System.out.println("\n1. Register to Class\n" +
                 "2. Logout");
     }
 
@@ -76,16 +77,76 @@ public class SMSRunner {
 
         if(flag) {
             while(true) {
-                System.out.printf("My classes:\n%10s %20s %20s", "#", "Course Name", "Instructor Name");
+                System.out.printf("My classes:\n%-10s %-40s %-20s", "#", "Course Name", "Instructor Name");
                 studentService.getStudentCourses(email).forEach(courses -> System.out.printf("%10d %40s %20s\n",
                         courses.getcId(),
                         courses.getcName(),
                         courses.getcInstructorName()));
-//                boolean logout = registerStudent(email); finish this line after creation of method
+                boolean logout = registerStudent(email);
+
+                if(logout == false) {
+                    System.out.println("You have been signed out");
+                    break;
+                } else {
+                    continue;
+                }
             }
         } else {
             System.out.println("Wrong Credentials");
         }
 
+    }
+
+    public static boolean registerStudent(String email) {
+        int choice = 0;
+        while(true) {
+            registerCourseMenu();
+            // try catch with if statement to catch InputMismatch error
+            try {
+                choice = input.nextInt();
+                input.nextLine();
+
+                // catch
+                if(choice == 1 || choice == 2) {
+                    break;
+                } else {
+                    System.out.println("Please enter 1 or 2");
+                }
+            } catch(InputMismatchException e) {
+                input.nextLine();
+                System.out.println("Please enter only 1 or 2");
+            }
+        }
+
+        switch(choice) {
+            case 1:
+                System.out.printf("All Courses:\n%-10s %-40s %-20s\n", "ID", "Course Name", "Instructor Name");
+                courseService.getAllCourses().forEach(courses -> System.out.printf("%-10d %-40s %-20s\n",
+                        courses.getcId(),
+                        courses.getcName(),
+                        courses.getcInstructorName()));
+                System.out.println("Select a Course:");
+                int courseOption;
+
+                while(true) {
+                    try {
+                        courseOption = input.nextInt();
+                        input.nextLine();
+
+                        if(courseOption > 0 && courseOption < 11) {
+                            break;
+                        } else {
+                            System.out.println("Please enter 1 - 10 only");
+                        }
+                    } catch(InputMismatchException e) {
+                        input.nextLine();
+                        System.out.println("Please only enter a valid number 1 - 10");
+                    }
+                }
+                studentService.registerStudentToCourse(email,courseOption);
+                return true;
+            default:
+                return false;
+        }
     }
 }
