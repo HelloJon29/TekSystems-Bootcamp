13938a04270f85aadadf2a29c6e2a7ec1ab18b8b
HelloJon29 <jonkim1996@gmail.com>
2022-03-28 09:28:16 -0500

    exercises on js DOM

---
+++ Practice/HTML&CSS/HTML/html-exercise/html-pages/js_exercises/print_shapes.js
@@ -0,0 +1,30 @@
+// var size = 6;
+
+// for (let i = 0; i < size; i++) {
+//   var tmp = ""
+//   for(let j = 0; j <= i; j++) {
+//    tmp += "*";
+//   } 
+//   console.log(tmp);
+// }
+
+// var size = 5;
+
+// for (let i = size; i >= 0; i--) {
+//   var tmp = ""
+//   for(let j = 0; j < i; j++) {
+//    tmp += "*";
+//   } 
+//   console.log(tmp);
+// }
+
+var size = 5
+
+for(let i = 0; i < size; i++) {
+  var star = "";
+  for(let j = 0; j <= i; j++) {
+    if(j<= size) {
+      "*"
+    } else " "
+  }
+}
